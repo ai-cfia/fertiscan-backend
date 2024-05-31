adede61a5c5b2a68b3b21f2e411d3ce2f6368a57
JulesB <62644404+Youllou@users.noreply.github.com>
2024-05-31 18:36:16 +0000

    Turn analyze into a polling request Fixes #37

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import os
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
 from backend import DocumentStorage, OCR, LanguageModel
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
-
+from threading import Thread
 # Load environment variables
 load_dotenv()
 
@@ -17,7 +17,7 @@ FRONTEND_URL = os.getenv('FRONTEND_URL')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # CORS configuration limited to the frontend URL
-cors = CORS(app,resources={"*",FRONTEND_URL}) 
+cors = CORS(app,resources={"*","*"}) 
 app.config['CORS_HEADERS'] = 'Content-Type'
 # Configuration for Azure Form Recognizer
 API_ENDPOINT = os.getenv('AZURE_API_ENDPOINT')
@@ -53,14 +53,21 @@ def upload_image():
         # Add image to document storage
         document_storage.add_image(file_path)
         
+        
+        global SHOULD_GET_DATA 
+        SHOULD_GET_DATA = False
+        global data 
+        data = ""
+        
         return "File uploaded successfully", 200
 
-@app.route('/analyze', methods=['GET'])
-def analyze_document():
-    document = document_storage.get_document()
-    if not document:
-        return "No documents to analyze", 400
+
+
+def get_data(document):
     
+    global SHOULD_GET_DATA
+    SHOULD_GET_DATA = True
+
     # For simplicity, only analyze the first document
     result = ocr.extract_text(document=document)
     
@@ -72,13 +79,37 @@ def analyze_document():
     # dict.pop('tables')
     # dict.pop('paragraphs')
     # form = language_model.generate_form(json.dumps(dict))
-    form = language_model.generate_form(dict['content'])
     
-    return app.response_class(
-        response=form,
-        status=200,
-        mimetype='application/json'
-    )
+    global data
+    data = language_model.generate_form(dict['content'])
+
+@app.route('/analyze', methods=['GET'])
+def analyze_document():
+    
+    document = document_storage.get_document()
+    if not document:
+        return "No documents to analyze", 400
+    
+    
+    
+    print(f"data from route pov : {data}")
+    if data == "":
+        if not SHOULD_GET_DATA :
+            Thread(target=lambda x=document: get_data(x)).start()
+
+        response = {
+            "Retry-after":10,
+        }
+        return jsonify(response)
+    
+    else:
+        return app.response_class(
+            response=data,
+            status=200,
+            mimetype='application/json'
+        )
+    
+    
     # return jsonify(form), 200
 
 if __name__ == "__main__":
