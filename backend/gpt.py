feda0f05a28f7fa71ef9cfb31a2bfcdadbb89983
James <60270865+Endlessflow@users.noreply.github.com>
2024-06-10 22:01:24 +0000

    feat: Add LLM processing and GPT class enhancements
    ### e2e.py
    - Implement LLM processing for different GPT models, adding functionality to process OCR outputs through LLMs.
    - Introduce a check to skip processing if the OCR or LLM output files already exist, reducing redundant API calls.
    
    ### backend/gpt.py
    - Update the `GPT` class to include optional debug logging for AzureOpenAI requests.
    - Refactor request parameter handling to accommodate models that support different response formats.
    - Add timing for AzureOpenAI requests in debug mode to help with performance monitoring.

--- backend/gpt.py
+++ backend/gpt.py
@@ -1,5 +1,7 @@
 import os
 from openai import AzureOpenAI
+import time
+
 
 class GPT:
     def __init__(self, api_endpoint, api_key, deployment="ailab-gpt-35-turbo-16k"):
@@ -13,16 +15,32 @@ class GPT:
             api_version = "2024-02-01",
         )
 
-    def generate_form(self, prompt):
+    def generate_form(self, prompt, DEBUG=False):
+        if DEBUG:
+            print(f"sending AzureOpenAI request to {self.model}...")
+            start_time = time.time()
+
         prompt_file = open(os.getenv('PROMPT_PATH'))
         system_prompt = prompt_file.read()
         prompt_file.close()
-        response = self.client.chat.completions.create(
-            model=self.model, # model = "deployment_name".
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt}
-            ],
-            temperature=0,
-        )
+        
+        models_with_response_format = ["ailab-llm"] # List of models that support the response_format option
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt}
+        ]
+        request_params = {
+            "model": self.model, # model = "deployment_name".
+            "messages": messages,
+            "temperature": 0,
+        }
+        if self.model in models_with_response_format:
+            request_params["response_format"] = { "type": "json_object" }
+        response = self.client.chat.completions.create(**request_params)
+
+        if DEBUG:
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"AzureOpenAI replied. Time taken: {elapsed_time:.2f} seconds.")
+        
         return response.choices[0].message.content
--- e2e.py
+++ e2e.py
@@ -7,53 +7,7 @@ from backend.gpt import GPT
 from backend import save_text_to_file
 from typing import Dict
 
-# Load environment variables
 load_dotenv()
-
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler("pipeline.log"),
-        logging.StreamHandler()
-    ]
-)
-logger = logging.getLogger(__name__)
-
-# Constants
-DOCUMENT_INTELLIGENCE_API_ENDPOINT = os.getenv('AZURE_API_ENDPOINT')
-DOCUMENT_INTELLIGENCE_API_KEY = os.getenv('AZURE_API_KEY')
-OPENAI_API_ENDPOINT = os.getenv('AZURE_OPENAI_ENDPOINT')
-OPENAI_API_KEY = os.getenv('AZURE_OPENAI_KEY')
-
-INPUT_IMAGES_PATH = 'e2e_tests/image_inputs'
-OCR_OUTPUTS_PATH = 'e2e_tests/ocr_outputs'
-LLM_OUTPUTS_PATH = 'e2e_tests/llm_outputs'
-
-
-def process_ocr(ocr_engines: Dict[str, OCR]) -> None:
-    for engine_name, ocr_engine in ocr_engines.items():
-        engine_output_dir = os.path.join(OCR_OUTPUTS_PATH, engine_name)
-        os.makedirs(engine_output_dir, exist_ok=True)
-
-        for img in os.listdir(INPUT_IMAGES_PATH):
-            try:
-                image_name, _ = os.path.splitext(os.path.basename(img))
-
-                document_storage.add_image(os.path.join(INPUT_IMAGES_PATH, img))
-                document = document_storage.get_document(format='pdf')
-
-                result = ocr_engine.extract_text(document=document)
-                output_filepath = os.path.join(engine_output_dir, f"ocr_{engine_name}_{image_name}.md")
-                save_text_to_file(result.content, output_filepath)
-
-                logger.info(f"Processed OCR for {img} with {engine_name}")
-            except Exception as e:
-                logger.error(f"Failed to process {img} with {engine_name}: {e}", exc_info=True)
-            finally:
-                document_storage.images = []
-
 
 def validate_env_vars():
     required_vars = [
@@ -67,15 +21,90 @@ def validate_env_vars():
         raise EnvironmentError(f"Missing required environment variables: {', '.join(missing_vars)}")
 
 
+DOCUMENT_INTELLIGENCE_API_ENDPOINT = os.getenv('AZURE_API_ENDPOINT')
+DOCUMENT_INTELLIGENCE_API_KEY = os.getenv('AZURE_API_KEY')
+OPENAI_API_ENDPOINT = os.getenv('AZURE_OPENAI_ENDPOINT')
+OPENAI_API_KEY = os.getenv('AZURE_OPENAI_KEY')
+
+INPUT_IMAGES_PATH = 'e2e_tests/image_inputs'
+OCR_OUTPUTS_PATH = 'e2e_tests/ocr_outputs'
+LLM_OUTPUTS_PATH = 'e2e_tests/llm_outputs'
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("pipeline.log"),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+
+def process_ocr(ocr_engines: Dict[str, OCR]) -> None:
+    for ocr_engine_name, ocr_engine in ocr_engines.items():
+        engine_output_dir = os.path.join(OCR_OUTPUTS_PATH, ocr_engine_name)
+        os.makedirs(engine_output_dir, exist_ok=True)
+
+        for img in os.listdir(INPUT_IMAGES_PATH):
+            image_name, _ = os.path.splitext(os.path.basename(img))
+            output_filepath = os.path.join(engine_output_dir, f"ocr_{ocr_engine_name}_{image_name}.md")
+
+            if os.path.exists(output_filepath):
+                logger.info(f"OCR output for {img} with {ocr_engine_name} already exists. Skipping processing.")
+                continue
+
+            try:
+                document_storage.add_image(os.path.join(INPUT_IMAGES_PATH, img))
+                document = document_storage.get_document(format='pdf')
+
+                result = ocr_engine.extract_text(document=document)
+                save_text_to_file(result.content, output_filepath)
+
+                logger.info(f"Processed OCR for {img} with {ocr_engine_name}")
+            except Exception as e:
+                logger.error(f"Failed to process {img} with {ocr_engine_name}: {e}", exc_info=True)
+            finally:
+                document_storage.images = []
+
+
+def process_llm(ocr_engines: Dict[str, OCR], llm_models: Dict[str, GPT]) -> None:
+    for ocr_engine_name, _ in ocr_engines.items():
+        ocr_output_dir = os.path.join(OCR_OUTPUTS_PATH, ocr_engine_name)
+        for llm_name, llm_model in llm_models.items():
+            llm_output_dir = os.path.join(LLM_OUTPUTS_PATH, llm_name)
+            os.makedirs(llm_output_dir, exist_ok=True)
+
+            for ocr_output_file in os.listdir(ocr_output_dir):
+                ocr_output_file_basename, _ = os.path.splitext(os.path.basename(ocr_output_file))
+                output_filename = f"llm_{llm_name}_{ocr_output_file_basename}.json"
+                llm_output_path = os.path.join(llm_output_dir, output_filename)
+
+                if os.path.exists(llm_output_path):
+                    logger.info(f"LLM output for {ocr_output_file} with {llm_name} already exists. Skipping processing.")
+                    continue
+
+                try:
+                    ocr_output_path = os.path.join(ocr_output_dir, ocr_output_file)
+                    with open(ocr_output_path, 'r') as file:
+                        ocr_content = file.read()
+                    
+                    llm_result = llm_model.generate_form(ocr_content)
+                    save_text_to_file(llm_result, llm_output_path)
+
+                    logger.info(f"Processed LLM for {ocr_output_file} with {llm_name}")
+                except Exception as e:
+                    logger.error(f"Failed to process {ocr_output_file} with {llm_name}: {e}", exc_info=True)
+
+
 if __name__ == "__main__":
     try:
         validate_env_vars()
         document_storage = DocumentStorage()
 
-        # OCR Engines
         document_intelligence = OCR(api_endpoint=DOCUMENT_INTELLIGENCE_API_ENDPOINT, api_key=DOCUMENT_INTELLIGENCE_API_KEY)
 
-        # LLMs
         gpt35 = GPT(api_endpoint=OPENAI_API_ENDPOINT, api_key=OPENAI_API_KEY, deployment='ailab-gpt-35-turbo-16k')
         gpt4 = GPT(api_endpoint=OPENAI_API_ENDPOINT, api_key=OPENAI_API_KEY, deployment="ailab-llm")
 
@@ -90,6 +119,9 @@ if __name__ == "__main__":
 
         process_ocr(ocr_engines)
         logger.info("OCR processing completed successfully for all engines.")
+
+        process_llm(ocr_engines, llm_models)
+        logger.info("LLM processing completed successfully for all models.")
     except EnvironmentError as e:
         logger.critical(f"Environment setup error: {e}")
     except Exception as e:
