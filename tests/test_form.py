dc52446cce621068b8c588ace77d24e09c413a0c
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2024-07-04 15:02:36 -0400

    fix: simplify form tests

--- tests/test_form.py
+++ tests/test_form.py
@@ -28,33 +28,17 @@ class TestFertiliserForm(unittest.TestCase):
             self.assertEqual(expected_value, value, f"Value for key '{key}' does not match. Expected '{expected_value}', got '{value}'")
 
     def test_invalid_npk_format(self):
-        invalid_data = {
-            "company_name": "ABC Company",
-            "fertiliser_name": "Super Fertiliser",
-            "npk": "invalid-format",  # invalid npk format
-            "instructions_en": ["Use as directed"],
-            "micronutrients_en": [{"nutrient": "Iron", "value": "2", "unit": "%"}],
-            "specifications_en": [{"humidity": "Low", "ph": "7", "solubility": "High"}],
-            "guaranteed_analysis": [{"nutrient": "Nitrogen", "value": "10", "unit": "%"}]
-        }
-
         with self.assertRaises(ValidationError):
-            FertiliserForm(**invalid_data)
+            FertiliserForm(npk="invalid-format")
 
     def test_valid_npk_format(self):
-        data = {
-            "company_name": "ABC Company",
-            "fertiliser_name": "Super Fertiliser",
-            "npk": "10.5-20-30",  # invalid npk format
-            "instructions_en": ["Use as directed"],
-            "micronutrients_en": [{"nutrient": "Iron", "value": "2", "unit": "%"}],
-            "specifications_en": [{"humidity": "Low", "ph": "7", "solubility": "High"}],
-            "guaranteed_analysis": [{"nutrient": "Nitrogen", "value": "10", "unit": "%"}]
-        }
-
         try:
-            FertiliserForm(**data)
+            FertiliserForm(npk="10.5-20-30")
+            FertiliserForm(npk="10.5-20.5-30")
+            FertiliserForm(npk="10.5-0.5-30.1")
+            FertiliserForm(npk="0-20.5-30.1")
         except ValidationError as e:
             self.fail(f"Validation error: {e}")
+            
 if __name__ == '__main__':
     unittest.main()
