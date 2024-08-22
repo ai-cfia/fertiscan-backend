65f34759c24c1b977a66cfae0bc6b4abee51f74e
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2024-08-22 13:51:23 -0400

    test: remove content header

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,7 +13,6 @@ class APITestCase(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.headers = {
-            'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Methods': '*',
