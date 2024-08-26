eaaa5f29595e304f57045cb98e533054a135f41d
Bryan Ndjeutcha <49378990+snakedye@users.noreply.github.com>
2024-08-26 16:21:32 +0000

    fix: store inspection data from create in the test class

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,12 +16,14 @@ from unittest.mock import patch, MagicMock
 test_client = app.test_client()
 
 class APITestCase(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         app.testing = True
         
         self.username = 'test'
         self.password = 'password1'
-        self.inspection_id = str(uuid.uuid4())
+        self.inspection = None
+        self.inspection_id = None
         encoded_credentials = self.credentials(self.username, self.password)
 
         unittest.TestLoader.sortTestMethodsUsing = None
@@ -32,7 +34,8 @@ class APITestCase(unittest.TestCase):
             'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Methods': '*',
         }
-
+        
+    @classmethod
     def credentials(self, username, password) -> str:
         credentials = f"{username}:{password}"
         encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
@@ -91,8 +94,7 @@ class APITestCase(unittest.TestCase):
             self.assertEqual(response.status_code, 201, response.json)
 
     def test_login_missing_username(self):
-        response = test_client.post('/login', headers={ **self.headers,  'Authorization': f'Basic {self.credentials("", self.password)}'},
-)
+        response = test_client.post('/login', headers={ **self.headers,  'Authorization': f'Basic {self.credentials("", self.password)}'},)
         self.assertEqual(response.status_code, 400, response.json)
     
     def test_login_unknow_username(self):
@@ -115,7 +117,6 @@ class APITestCase(unittest.TestCase):
         response = test_client.post('/inspections', headers=self.headers)
         self.assertEqual(response.status_code, 500, response.json)
 
-
     # If the inspection is created, it should at least return a message testifying that.
     def test_create_inspection(self):
         with requests.get('https://raw.githubusercontent.com/ai-cfia/fertiscan-pipeline/main/expected.json') as response:
@@ -127,8 +128,9 @@ class APITestCase(unittest.TestCase):
                 json=json_data
             )
             self.assertEqual(response.status_code, 201, response.json)
-            self.inspection_id = response.json['inspection_id']
-
+            self.__class__.inspection_id = response.json['inspection_id']
+            self.__class__.inspection = response.json
+            
     def test_update_empty_inspection(self):
         inspection_id = str(uuid.uuid4())
         response = test_client.put(f'/inspections/{inspection_id}', headers=self.headers)
@@ -136,26 +138,31 @@ class APITestCase(unittest.TestCase):
 
     # Requires the database schema
     def test_update_inspection_fake_id(self):
+        # Ensure that `test_create_inspection` has run and set the inspection data
+        self.assertIsNotNone(self.inspection, "Inspection data should not be None")
+      
         inspection_id = str(uuid.uuid4()) # Should fail since the inspection with this id does not exist
         response = test_client.put(
             f'/inspections/{inspection_id}', 
             headers=self.headers,
             content_type='application/json',
-            json={'status': 'completed'}
+            json=self.inspection
         )
-        self.assertEqual(response.status_code, 400, response.json)
+        self.assertEqual(response.status_code, 500, response.json)
 
     # Requires the database schema
     def test_update_inspection(self):
-        with requests.get('https://raw.githubusercontent.com/ai-cfia/fertiscan-pipeline/main/expected.json') as response:
-            json_data = response.json()
-            response = test_client.put(
-                f'/inspections/{self.inspection_id}', 
-                headers=self.headers,
-                content_type='application/json',
-                json=json_data
-            )
-            self.assertEqual(response.status_code, 200, response.json)
+        # Ensure that `test_create_inspection` has run and set the inspection data
+        self.assertIsNotNone(self.inspection_id, "Inspection ID should not be None")
+        self.assertIsNotNone(self.inspection, "Inspection data should not be None")
+        
+        response = test_client.put(
+            f'/inspections/{self.inspection_id}', 
+            headers=self.headers,
+            content_type='application/json',
+            json=self.inspection
+        )
+        self.assertEqual(response.status_code, 200, response.json)
     
     def test_get_inspection_from_unknown_user(self):
         username = str(uuid.uuid4())
