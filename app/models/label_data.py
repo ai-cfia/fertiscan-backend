32e5aeb95f365f41e4077a8fb3f68353369c5859
Bryan Ndjeutcha <albert.ndjeutcha@inspection.gc.ca>
2025-02-03 11:31:23 -0500

    fix: return in validation

--- app/models/label_data.py
+++ app/models/label_data.py
@@ -11,10 +11,13 @@ class LabelData(FertilizerInspection):
             return cls.model_validate_json(value)
         return value
     
-    @model_validator(mode="after")
+    @model_validator(mode="before")
     @classmethod
     def validate_registration_number(cls, value):
-        inspection = cls.model_validate_json(value)
-        for registration_number in inspection.registration_number:
-            if not re.match(r"\d+(\.\d+)?", registration_number.identifier):
-                raise ValueError(f"Invalid registration number: {registration_number.identifier}")
+        if value is not None:
+            inspection = FertilizerInspection.model_validate(value)
+            for registration_number in inspection.registration_number:
+                if registration_number.identifier is not None:
+                    if not re.match(r"\d+(\.\d+)?", registration_number.identifier):
+                        raise ValueError(f"Invalid registration number: {registration_number.identifier}")
+        return value
