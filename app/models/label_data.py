999b3a3d03fd1d5b7f400591fc84e01f9e0cb3ef
Bryan Ndjeutcha <49378990+snakedye@users.noreply.github.com>
2025-02-17 10:24:53 -0500

    Issue #233 : Throw an error if the registration number does not match a pattern (#234)
    * fix: update .gitignore to include uploads directory and add registration number validation in LabelData model
    
    * fix: return in validation

--- app/models/label_data.py
+++ app/models/label_data.py
@@ -1,3 +1,4 @@
+import re
 from pipeline import FertilizerInspection
 from pydantic import model_validator
 
@@ -8,4 +9,15 @@ class LabelData(FertilizerInspection):
     def validate_to_json(cls, value):
         if isinstance(value, str):
             return cls.model_validate_json(value)
+        return value
+    
+    @model_validator(mode="before")
+    @classmethod
+    def validate_registration_number(cls, value):
+        if value is not None:
+            inspection = FertilizerInspection.model_validate(value)
+            for registration_number in inspection.registration_number:
+                if registration_number.identifier is not None:
+                    if not re.match(r"\d+(\.\d+)?", registration_number.identifier):
+                        raise ValueError(f"Invalid registration number: {registration_number.identifier}")
         return value
